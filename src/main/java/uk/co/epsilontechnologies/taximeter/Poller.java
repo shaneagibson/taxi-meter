4d487847818678b73b3683f7e054e5f3a7410f85
Shane Gibson <shane.a.gibson@gmail.com>
2013-11-24 19:14:33 +0000

    Updated documentation

--- src/main/java/uk/co/epsilontechnologies/taximeter/Poller.java
+++ src/main/java/uk/co/epsilontechnologies/taximeter/Poller.java
@@ -8,6 +8,12 @@ import java.util.concurrent.TimeUnit;
 /**
  * <p>Wrapper of a Scheduled Executor Service to provide polling every tenth of a second for updating the taxi meter.
  *
+ * <p>By polling every 1/10 of a second, the taxi meter will ensure accurate fares, since the rules of TfL's Tariffs
+ * use time limits down to the tenth of a second. The shortest distance at which rates are incremented is 89.2 metres,
+ * which equates to a window of 892 metres per second, or 3211 kilometres per hour - much faster than the standard
+ * London Taxi. Therefore there should be no need to recursively or iteratively increment the fare in a single
+ * calculation.
+ *
  * @author Shane Gibson
  */
 public class Poller {
