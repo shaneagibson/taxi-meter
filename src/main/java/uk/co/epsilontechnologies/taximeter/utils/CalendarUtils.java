e1b1a6f22bf08d623f320c12c7794622e2c2a25c
Shane Gibson <shane.a.gibson@gmail.com>
2013-11-24 20:21:20 +0000

    Updated documentation. Moved public holidays into txt file.

--- src/main/java/uk/co/epsilontechnologies/taximeter/utils/CalendarUtils.java
+++ src/main/java/uk/co/epsilontechnologies/taximeter/utils/CalendarUtils.java
@@ -22,25 +22,23 @@ public final class CalendarUtils {
     /**
      * List of UK public holidays for 2013, 2014, and 2015.
      */
-    private static final List<LocalDate> PUBLIC_HOLIDAYS = new ArrayList<>();
-
-    static {
-        loadPublicHolidays();
-    }
+    private static final List<LocalDate> PUBLIC_HOLIDAYS = loadPublicHolidays();
 
     /**
      * Loads the public holidays from the publicholidays.txt file into an in-memory list.
      */
-    private static void loadPublicHolidays() {
+    private static List<LocalDate> loadPublicHolidays() {
         try (final InputStream publicHolidaysTxtInputStream = CalendarUtils.class.getClassLoader().getResourceAsStream("publicholidays.txt")) {
             final List<String> lines = IOUtils.readLines(publicHolidaysTxtInputStream);
+            final List<LocalDate> list = new ArrayList<>();
             for (final String line : lines) {
                 final StringTokenizer stringTokenizer = new StringTokenizer(line, ",");
                 final int year = Integer.parseInt(stringTokenizer.nextToken());
                 final int month = Integer.parseInt(stringTokenizer.nextToken());
                 final int day = Integer.parseInt(stringTokenizer.nextToken());
-                PUBLIC_HOLIDAYS.add(new LocalDate(year, month, day));
+                list.add(new LocalDate(year, month, day));
             }
+            return list;
         } catch (final IOException e) {
             throw new RuntimeException("Unable to load public holidays: "+e);
         }
